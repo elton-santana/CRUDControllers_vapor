52406b83a155c68f55acade1f76fa40820e84f98
Elton Santana <faletaelton@gmail.com>
2019-09-17 14:56:38 -0300

    Exposed routes and called related UserController methods

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,15 +2,27 @@ import Vapor
 
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
-    // Basic "It works" example
-    router.get { req in
-        return "It works!"
-    }
+    let usersController = UsersController()
+    try router.register(collection: usersController)
     
-    // Basic "Hello, world!" example
-    router.get("hello") { req in
-        return "Hello, world!"
+    router.post("user") { req in
+        return try usersController.createHandler(req)
     }
 
- 
+    router.get("users") { req in
+        return try usersController.getAllHandler(req)
+    }
+    
+    router.get("user") { req in
+        return try usersController.getOneHandler(req)
+    }
+
+    router.put("user") { req in
+        return try usersController.updateHandler(req)
+    }
+    
+    router.delete("user") { req in
+        return try usersController.deleteHandler(req)
+    }
+
 }
