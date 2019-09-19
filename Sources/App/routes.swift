9a3da8663f2f146ba3da7c86094e3a01633b4128
Elton Santana <faletaelton@gmail.com>
2019-09-19 14:20:39 -0300

    Registered properly UsersControllers routes

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -3,26 +3,11 @@ import Vapor
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
     let usersController = UsersController()
-    try router.register(collection: usersController)
-    
-    router.post("user") { req in
-        return try usersController.createHandler(req)
+    do{
+        try router.register(collection: usersController)
+    } catch(let error) {
+        print(error.localizedDescription)
     }
 
-    router.get("users") { req in
-        return try usersController.getAllHandler(req)
-    }
-    
-    router.get("user") { req in
-        return try usersController.getOneHandler(req)
-    }
-
-    router.put("user") { req in
-        return try usersController.updateHandler(req)
-    }
-    
-    router.delete("user") { req in
-        return try usersController.deleteHandler(req)
-    }
 
 }
